e684d5f3aa2961abd75d016cee8f285d4f09e33a
Mateusz Kurzawa <mateuszk91@op.pl>
2023-02-08 13:43:45 +0100

    buttons + hover + disabled 13:40 08-02-2023

--- src/inputs/Button/Button.stories.tsx
+++ src/inputs/Button/Button.stories.tsx
@@ -1,13 +1,21 @@
 // Button.stories.tsx
-import { Meta, Story } from "@storybook/preact";
-import { h } from "preact";
-import { Button } from "./Button";
-import * as React from "preact/compat";
+import { Args, Meta, Story } from '@storybook/preact';
+import { h } from 'preact';
+import { Button } from './Button';
+import * as React from 'preact/compat';
 
 export default {
+  tittle: 'Button',
   component: Button,
 } as Meta;
 
-export const Primary: Story = () => <Button>Click me</Button>;
+export const Primary: Story = (args: Args) => (
+  <Button {...args} onClick={() => {}}>
+    Click me
+  </Button>
+);
 
-Primary.args = {};
+Primary.args = {
+  disabled: false,
+  variant: 'primary',
+};
--- src/inputs/Button/Button.tsx
+++ src/inputs/Button/Button.tsx
@@ -1,10 +1,43 @@
 import { PropsWithChildren } from 'preact/compat';
 import { h } from 'preact';
+import React from 'react';
 
 import * as styles from './Button.module.css';
+import { Primary } from './Button.stories';
 
-interface ButtonProps {}
+interface ButtonProps {
+  type?: 'button' | 'submit';
+  disabled?: boolean;
+  icon?: React.ReactElement;
+  isLoading?: boolean;
+  onClick(): void;
+  variant?: 'primary' | 'secondary';
+}
 
-export const Button = (props: PropsWithChildren<ButtonProps>) => {
-  return <button>{props.children}</button>;
+export const Button = ({
+  disabled = false,
+  variant = 'primary',
+  ...props
+}: PropsWithChildren<ButtonProps>) => {
+  const variantClass =
+    variant === 'primary' ? styles.primary : styles.secondary;
+
+  const getDisabledClass = (): string => {
+    if (disabled) {
+      return variant === 'primary'
+        ? styles.disabledPrimary
+        : styles.disabledSecondary;
+    }
+    return '';
+  };
+
+  const classNames = [styles.template, variantClass, getDisabledClass()].join(
+    ' '
+  );
+
+  return (
+    <button className={classNames} onClick={props.onClick} disabled={disabled}>
+      {props.children}
+    </button>
+  );
 };
